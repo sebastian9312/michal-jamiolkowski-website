6e4d33bc3ae5a3d51b7db8d0a7d03f741c27f976
sebastian9312 <96694150+sebastian9312@users.noreply.github.com>
2023-01-28 17:07:05 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `michal-jamiolkowski-website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
